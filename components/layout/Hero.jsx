f07a00453bdde8d06a6281de905b2e5fd6815978
nziku <amosisanga04>
2026-01-23 15:19:26 +0300

    font changed

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -15,10 +15,10 @@ const Hero = () => {
       {/* Video background without object-cover */}
       <div className="absolute inset-0 z-0 flex justify-center items-center">
         {!isVideoLoaded ? (
-                            <div className="text-red-500 text-center">
-                              <p className="w-6 h-6 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></p>
-                            </div>
-                          ) : null}
+          <div className="text-red-500 text-center">
+            <p className="w-6 h-6 border-4 border-red-500 border-t-transparent rounded-full animate-spin"></p>
+          </div>
+        ) : null}
         <video
           src="/assets/videos/dan.mp4"
           className="object-cover absolute top-1/2 left-1/2 min-w-full min-h-full transform -translate-x-1/2 -translate-y-1/2"
@@ -27,7 +27,7 @@ const Hero = () => {
           muted
           playsInline
           preload="auto"
-          onLoadedData={() => setIsVideoLoaded(true) }
+          onLoadedData={() => setIsVideoLoaded(true)}
         />
       </div>
 
@@ -42,15 +42,14 @@ const Hero = () => {
       {
         isVideoLoaded && (
           <div
-          style={{ fontFamily: "libel" }}
-          className="absolute z-20 inset-0 flex flex-col justify-center items-center text-center text-white uppercase font-bold text-2xl sm:text-4xl">
-        <p>Hello!</p>
-        <p>Welcome To The World Of</p>
-        <div className="flex gap-1 justify-center items-center">
-          <p className="text-red-400 font-bold">{text}</p>
-          <Cursor cursorColor="rgb(248,113,113)" cursorStyle="|" />
-        </div>
-      </div>
+            className="absolute z-20 inset-0 flex flex-col justify-center items-center text-center text-white uppercase font-bold text-2xl sm:text-4xl">
+            <p>Hello!</p>
+            <p>Welcome To The World Of</p>
+            <div className="flex gap-1 justify-center items-center">
+              <p className="text-red-400 font-bold">{text}</p>
+              <Cursor cursorColor="rgb(248,113,113)" cursorStyle="|" />
+            </div>
+          </div>
         )
       }
     </div>
